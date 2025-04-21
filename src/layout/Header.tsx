e1620f6b893e38350b744e66e78c5ead6e2bc60d
Seulji-jo <joseuling@gmail.com>
2025-04-21 15:08:41 +0900

    feature: 메뉴 안보이는 부분 active상태일때 화면에 보이게 기능추가

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,9 +1,29 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 import NavList from '../components/NavList';
 import { FreeMode } from 'swiper/modules';
 import { routeMap } from '../config/routeConfig';
+import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router';
 
 export default function Header() {
+  const swiperRef = useRef<SwiperClass | null>(null);
+  const location = useLocation();
+  const activeIdx = routeMap.findIndex(el => el.path === location.pathname);
+
+  useEffect(() => {
+    if (!swiperRef.current) return;
+
+    const swiper = swiperRef.current;
+    const activeSlide = swiper.slides[activeIdx];
+
+    if (activeSlide && swiper.wrapperEl) {
+      activeSlide.scrollIntoView({
+        behavior: 'smooth',
+        inline: 'nearest',
+      });
+    }
+  }, [activeIdx]);
+
   return (
     <header className="h-[45px] bg-[#FFB1B2]">
       <nav className="h-full">
@@ -12,6 +32,8 @@ export default function Header() {
           freeMode={true}
           modules={[FreeMode]}
           slidesPerView={'auto'}
+          watchSlidesProgress
+          onSwiper={swiper => (swiperRef.current = swiper)}
         >
           {routeMap.map(({ path, name }, i) => (
             <SwiperSlide
